bc3b44e0153604c11edc74740f079d249eea512e
TakedaKaho <tktk.a3104c@gmail.com>
2024-06-15 13:37:50 +0000

    2 ログイン完了（再）

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,11 +1,8 @@
 class Admin::HomesController < ApplicationController
-<<<<<<< HEAD
   before_action :authenticate_admin!
   
    def top
    end 
    def destroy
    end 
-=======
->>>>>>> ff8825c656bbaed323d8e88fed4f85790853a97e
 end
