ea8c868739f055e43d331d2bc78734006ddf7a7b
EC2 Default User <ec2-user@ip-172-31-17-123.ap-northeast-1.compute.internal>
2024-06-15 06:36:59 +0000

    1 2 db:カラム記入完了

--- db/migrate/20240615050643_create_orders.rb
+++ db/migrate/20240615050643_create_orders.rb
@@ -1,8 +1,17 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
-
-      t.timestamps
+    t.integer :payment_method, null: false
+    t.string :name, null: false
+    t.string :post_code, null: false
+    t.string :address, null: false
+    t.integer :status, null: false
+    t.integer :shopping_cost, null: false
+    t.integer :total_payment, null: false
+    
+   
+    t.timestamps
     end
   end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_06_15_041537) do
+ActiveRecord::Schema.define(version: 2024_06_15_053228) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "post_code", null: false
+    t.string "address", null: false
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,7 +32,21 @@ ActiveRecord::Schema.define(version: 2024_06_15_041537) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "amount", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "customers", force: :cascade do |t|
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "phone_number", null: false
+    t.string "post_code", null: false
+    t.string "address", null: false
+    t.boolean "is_active", default: true, null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -34,6 +56,41 @@ ActiveRecord::Schema.define(version: 2024_06_15_041537) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "introduction", null: false
+    t.integer "price", null: false
+    t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_datails", force: :cascade do |t|
+    t.integer "amount", null: false
+    t.integer "price", null: false
+    t.integer "maiking_status", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "payment_method", null: false
+    t.string "name", null: false
+    t.string "post_code", null: false
+    t.string "address", null: false
+    t.integer "status", null: false
+    t.integer "shopping_cost", null: false
+    t.integer "total_payment", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
