ff8825c656bbaed323d8e88fed4f85790853a97e
EC2 Default User <ec2-user@ip-172-31-17-123.ap-northeast-1.compute.internal>
2024-06-15 07:35:54 +0000

    1 2 コントローラー作成完了

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,46 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+
+scope module: :public do
+  root :to =>"homes#top"
+  get '/about' => 'homes#about'
+  resources :items, only: [:index, :show]
+  resources :customers, only: [:show, :edit, :update] do
+    collection do
+      get 'check'
+      patch 'out'
+    end
+  end
+
+  resources :cart_items, only: [:index, :update, :create,:destroy] do
+    collection do
+      delete 'destroy_all'
+    end
+  end
+
+  resources :orders, only: [:new, :create, :index, :show] do
+    collection do
+      get 'thanks'
+      post "check"
+    end
+  end
+
+  resources :addresses, except: [:new, :show]
+  end
+
+
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
+
+  namespace :admin do
+    get '/' => 'homes#top'
+    resources :items, except: [:destroy]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
