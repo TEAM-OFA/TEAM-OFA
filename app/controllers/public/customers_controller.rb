d66f500b3e865027ec8e4db76b36d94b1a4305cc
EC2 Default User <ec2-user@ip-172-31-17-123.ap-northeast-1.compute.internal>
2024-06-16 15:08:42 +0000

    1 マイページ作成　途中

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -9,9 +9,10 @@ class Public::CustomersController < ApplicationController
     @customer.update(customer_params)
     redirect_to root_path
   end
-　
-　def show
-　end
+
+  def show
+    @customer = current_customer
+  end 
 
 
 
@@ -30,4 +31,6 @@ class Public::CustomersController < ApplicationController
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :post_code, :address, :phone_number, :email)
   end
 
+
 end
+
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -23,17 +23,19 @@ class Public::SessionsController < Devise::SessionsController
 
   protected
 
-  def reject_user
-    @user = User.find_by(name: params[:user][:name])
-    if @user
-      if @user.valid_password?(params[:user][:password]) && (@user.is_deleted == true)
+  def reject_customer
+    @customer = Customer.find_by(email: params[:customer][:email])
+    if @customer
+      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true)
         flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
-        redirect_to new_user_registration
+        redirect_to new_user_registration_path
       else
         flash[:notice] = "項目を入力してください"
       end
     end
   end
+
+
 
 
 
