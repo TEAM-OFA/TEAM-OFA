cb7bb4acef4e5d2fa215bbc988813a5b111beb4e
EC2 Default User <ec2-user@ip-172-31-46-143.ap-northeast-1.compute.internal>
2024-06-19 13:38:18 +0000

    1注文履歴途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,9 +8,11 @@ class Public::OrdersController < ApplicationController
   end
 
   def check
-    @cart_items = current_customer.cart_items.all
+    @cart_items = current_customer.cart_items
     @customer = current_customer
-    @order = Order.new
+    @order = Order.new(order_params)
+    @shopping_cost = 800
+    @order.status = 0
     @selected_payment_method = params[:order][:payment_method]
 
     address_option = params[:address_option]
@@ -18,13 +20,19 @@ class Public::OrdersController < ApplicationController
     case address_option
       when "0"
         @selected_address = "〒" + @customer.post_code + "　" + @customer.address + "　" + @customer.last_name + @customer.first_name
+        @order.address = @customer.address
+        @order.name = @customer.last_name + @customer.first_name
+        @order.post_code = @customer.post_code
       when "1"
-        unless params[:order][:registered_address_id] == ""
+        # unless params[:order][:registered_address_id] == ""
           selected = Address.find(params[:order][:address_id])
           @selected_address = "〒" + selected.post_code + "　" + selected.address + "　" + selected.name
-    	  else
-    	  render :new
-    	  end
+          @order.address = selected.address
+          @order.name = selected.name
+          @order.post_code = selected.post_code
+    	 # else
+    	 # render :new
+    	 # end
       when "2"
         unless params[:order][:post_code] == "" && params[:order][:address] == "" && params[:order][:name] == ""
 	        @selected_address = "〒" + params[:order][:post_code] + "　" + params[:order][:address] + "　" + params[:order][:name]
@@ -34,19 +42,38 @@ class Public::OrdersController < ApplicationController
       else
         render :new
       end
-
+      p @order
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.save
+
+    if params[:address_option] == "2"
+      current_customer.address.create(address_params)
+    end
+
+    @cart_items = current_customer.cart_items
+    @cart_items.destroy_all
+
+    redirect_to thanks_orders_path
+
   end
 
   def index
+    @orders = current_customer.orders
   end
 
   def show
   end
 
   def thanks
+  end
+
+private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :payment_method, :post_code, :status, :shopping_cost, :total_payment, :address, :name)
   end
 
 end
