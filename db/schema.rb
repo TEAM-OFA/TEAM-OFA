2333377bcf38501d8591920d5703a081d86d7fc3
iamazukidayo <xxxxxxmiyu@gmail.com>
2024-06-20 06:46:05 +0000

    2 注文履歴詳細　途中です。。

--- db/schema.rb
+++ db/schema.rb
@@ -128,3 +128,4 @@ ActiveRecord::Schema.define(version: 2024_06_15_053228) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
+
