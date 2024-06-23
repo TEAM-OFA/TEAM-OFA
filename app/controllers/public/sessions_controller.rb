d9833f738ac25a4f58c7175a7f08bc5a94621ac2
TakedaKaho <tktk.a3104c@gmail.com>
2024-06-23 08:41:01 +0000

    ログイン遷移先修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,17 +24,22 @@ class Public::SessionsController < Devise::SessionsController
   protected
 
   def reject_customer
-   @customer = Customer.find_by(email: params[:customer][:email])
+  @customer = Customer.find_by(email: params[:customer][:email])
 
-   if @customer && @customer.valid_password?(params[:customer][:password]) && !@customer.is_active
-     flash[:withdrawal_notice] = "退会済みです。再度ご登録をしてご利用ください。"
-    redirect_to new_customer_registration_path
-   else
-     flash[:login_notice]= "該当するユーザが見当たりません。"
-     redirect_to new_customer_session_path
-   end
+  if @customer && @customer.valid_password?(params[:customer][:password])
+    if @customer.is_active
+      sign_in(@customer) # ログイン処理を実行
+      redirect_to root_path
+    else
+      flash[:withdrawal_notice] = "退会済みです。再度ご登録をしてご利用ください。"
+      redirect_to new_customer_registration_path
+    end
+  else
+    # 該当するユーザが見つからない場合やパスワードが違う場合の処理
+    flash[:login_notice] = "メールアドレスまたはパスワードが正しくありません。"
+    redirect_to new_customer_session_path
   end
-
+ end
 
 
 
